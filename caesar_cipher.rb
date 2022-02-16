faf150458a3c9d1ae59bf9ee163b630101e27671
Intimaria <intimaria@gmail.com>
2022-02-16 17:51:43 -0300

    initial commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,15 @@
+def convert(start, finish, n)
+  arr = (start..finish).to_a
+  arr.zip(arr.rotate(n)).to_h
+end
+
+def rotated_alphabet(n)
+  convert("a","z", n).merge(convert("A","Z",n))
+end 
+
+def caesar_cipher(string, n)
+  code = rotated_alphabet(n)
+  string.chars.map { |c| code.fetch(c, c)}.join
+end
+
+
